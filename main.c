90b9c8ee7e403baef785e5c7b3c6259f942f0791
Bernardozomer <bernardobarzottoz@gmail.com>
2022-06-25 16:59:35 -0300

    refactor: add field struct

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "chars.h"
@@ -29,90 +30,61 @@
 #define FLAG PIND &(1 << PD6)
 #define CHECK PIND &(1 << PD7)
 
+typedef struct field
+{
+	int mine;
+	int flagged;
+	int revealed;
+} Field;
+
 int playing = 0;
 int lost = 0;
 int min, sec = 0;
 int num_flags = 0;
 int seed = 0;
-char field[FIELD_HEIGHT][FIELD_WIDTH];
-char field_view[FIELD_HEIGHT][FIELD_WIDTH];
+Field board[FIELD_HEIGHT][FIELD_WIDTH];
 uint8_t sel_x = 0;
 uint8_t sel_y = 0;
 
-enum Tile
-{
-	Clock = 'a',
-	Blank = 'b',
-	Selected = 'c',
-	Flag = 'd',
-	Mine = 'e',
-};
-
+void reveal_board();
+void generate_mines();
+void handle_buttons(Field sel_field);
+void handle_movement();
+int move_overflowing(uint8_t sel, int x, int amount);
+void reset_board();
+void setup();
+void write_board();
+void write_flag_count(uint8_t x, uint8_t y);
 void write_start();
 void write_timer(uint8_t x, uint8_t y);
-void write_flag_count(uint8_t x, uint8_t y);
-void write_field();
-void reset_field();
-void generate_mines();
-int move_overflowing(uint8_t sel, int x, int amount);
 
-int main(void)
+void main()
 {
-	cli();
+	setup();
 
-	// Set up the timer.
-	TCCR1A = 0;
-	TCCR1B = 0;
-	TCNT1 = 0;
-	OCR1A = (TIMER_CLK / TIMER_FREQ) - 1;
-	TCCR1B |= (1 << WGM12);
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-	TIMSK1 |= (1 << OCIE1A);
+	while (1) {
+		while (!playing) {
+			nokia_lcd_clear();
 
-	// Set ports as input.
-	// These will be mapped to the buttons.
-	DDRD &= ~(1 << PD1) & ~(1 << PD2) & ~(1 << PD3) & ~(1 << PD4) & ~(1 << PD6) & ~(1 << PD7);
-
-	// Toggle interruption vector for PD7, ..., PD0.
-	PCICR |= (1 << PCIE2);
-	// Toggle interruptions for every button.
-	PCMSK2 |= (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD6) | (1 << PD7);
-
-	sei();
-
-	nokia_lcd_init();
-	nokia_lcd_custom(1, CLOCK_GLYPH);
-	nokia_lcd_custom(2, SPACE_GLYPH);
-	nokia_lcd_custom(3, SELECTED_GLYPH);
-	nokia_lcd_custom(4, FLAG_GLYPH);
-	nokia_lcd_custom(5, MINE_GLYPH);
-
-	while (1)
-	{
-		while (!playing)
-		{
-			if (!lost)
-			{
+			if (!lost) {
 				write_start();
 				seed++;
-				nokia_lcd_render();
-			}
-			else
-			{
-				write_end();
+			} else {
+				reveal_board();
+				write_board();
 				nokia_lcd_set_cursor(0, FIELD_HEIGHT * 8);
-				nokia_lcd_write_string("  Press FLAG", 1);
-				nokia_lcd_render();
+				nokia_lcd_write_string("  Press FLAG  ", 1);
 			}
+
+			nokia_lcd_render();
 		}
 
-		nokia_lcd_clear();
 		srand(seed);
-		reset_field();
+		reset_board();
 
-		while (playing)
-		{
-			write_field();
+		while (playing) {
+			nokia_lcd_clear();
+			write_board();
 			write_timer(0, FIELD_HEIGHT * 8);
 			write_flag_count(FIELD_WIDTH * 5 - 22, FIELD_HEIGHT * 8);
 			nokia_lcd_render();
@@ -124,14 +96,11 @@ int main(void)
  * Resets the field and the view to its initial state.
  * Mines are then regenerated.
  */
-void reset_field()
+void reset_board()
 {
-	for (int i = 0; i < FIELD_HEIGHT; i++)
-	{
-		for (int j = 0; j < FIELD_WIDTH; j++)
-		{
-			field[i][j] = '0';
-			field_view[i][j] = Blank;
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			board[i][j] = (Field) {0, 0, 0};
 		}
 	}
 
@@ -143,14 +112,12 @@ void reset_field()
  */
 void generate_mines()
 {
-	for (int mines = 0; mines < MINE_AMOUNT;)
-	{
+	for (int mines = 0; mines < MINE_AMOUNT;) {
 		int i = rand() % FIELD_HEIGHT;
 		int j = rand() % FIELD_WIDTH;
 
-		if (field[i][j] != Mine)
-		{
-			field[i][j] = Mine;
+		if (!board[i][j].mine) {
+			board[i][j].mine = 1;
 			mines++;
 		}
 	}
@@ -181,41 +148,31 @@ void write_flag_count(uint8_t x, uint8_t y)
 /**
  * Write the minefield matrix to the screen.
  */
-void write_field()
+void write_board()
 {
-	for (int i = 0; i < FIELD_HEIGHT; i++)
-	{
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		// Set the cursor to the start of the line.
 		nokia_lcd_set_cursor(0, i * 8);
-		for (int j = 0; j < FIELD_WIDTH; j++)
-		{
-			if (i == sel_y && j == sel_x && !lost)
-			{
-				nokia_lcd_write_string("\003", 1);
-			}
-			else
-			{
-				switch (field_view[i][j])
-				{
-				case Clock:
-					nokia_lcd_write_string("\001", 1);
-					continue;
-				case Blank:
-					nokia_lcd_write_string("\002", 1);
-					continue;
-				case Flag:
-					nokia_lcd_write_string("\004", 1);
-					continue;
-				case Mine:
-					nokia_lcd_write_string("\005", 1);
-					continue;
-				case '0':
-					nokia_lcd_write_string(" ", 1);
-					continue;
-				}
 
-				nokia_lcd_write_string(
-					field_view[1][j] - '0',
-					1);
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			if (i == sel_y && j == sel_x && !lost) {
+				nokia_lcd_write_string("\003", 1);
+				continue;
+			}
+
+			Field field = board[i][j];
+
+			if (!field.revealed) {
+				if (field.flagged) {
+					nokia_lcd_write_string("\004", 1);
+				} else {
+					nokia_lcd_write_string("\002", 1);
+				}
+			} else if (field.mine) {
+				nokia_lcd_write_string("\005", 1);
+			} else {
+				// TODO: write number of neighbouring bombs.
+				nokia_lcd_write_string(" ", 1);
 			}
 		}
 	}
@@ -226,28 +183,75 @@ void write_field()
  */
 void write_start()
 {
-	nokia_lcd_clear();
 	nokia_lcd_write_string(
 		"              "
 		" AVR \005 Mines! "
 		"              "
 		" Press CHECK! "
 		"			   ",
-		1);
+		1
+	);
 }
 
-void write_end()
+/**
+ * Reveal the whole board.
+ */
+void reveal_board()
 {
-	nokia_lcd_clear();
-	for (int i = 0; i < FIELD_HEIGHT; i++)
-	{
-		for (int j = 0; j < FIELD_WIDTH; j++)
-		{
-			field_view[i][j] = field[i][j];
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			board[i][j].revealed = 1;
 		}
 	}
-	field_view[sel_y][sel_x] = Mine;
-	write_field();
+}
+
+void handle_buttons(Field sel_field)
+{
+	if (CHECK) {
+		if (!playing && !lost) {
+			sec = 0;
+			min = 0;
+			playing = 1;
+		} else {
+			sel_field.revealed = 1;
+
+			if (sel_field.mine) {
+				lost = 1;
+				playing = 0;
+			} else {
+				num_flags = sel_field.flagged ? num_flags - 1 : num_flags;
+				sel_field.flagged = 0;
+			}
+		}
+	} else if (FLAG) {
+		if (!playing && lost) {
+			lost = 0;
+			sel_y = 0;
+			sel_x = 0;
+			num_flags = 0;
+		} else if (!sel_field.revealed) {
+			if (sel_field.flagged) {
+				sel_field.flagged = 0;
+				num_flags--;
+			} else {
+				sel_field.flagged = 1;
+				num_flags++;
+			}
+		}
+	}
+}
+
+void handle_movement()
+{
+	if (UP) {
+		sel_y = move_overflowing(sel_y, 0, -1);
+	} else if (DOWN) {
+		sel_y = move_overflowing(sel_y, 0, 1);
+	} else if (LEFT) {
+		sel_x = move_overflowing(sel_x, 1, -1);
+	} else if (RIGHT) {
+		sel_x = move_overflowing(sel_x, 1, 1);
+	}
 }
 
 /**
@@ -256,24 +260,18 @@ void write_end()
  */
 int move_overflowing(uint8_t sel, int x, int amount)
 {
-	if (amount < 0 && sel < abs(amount))
-	{
+	if (amount < 0 && sel < abs(amount)) {
 		return x ? FIELD_WIDTH - 1 : FIELD_HEIGHT - 1;
 	}
 
 	sel += amount;
 
-	if (x)
-	{
-		if (sel >= FIELD_WIDTH)
-		{
+	if (x) {
+		if (sel >= FIELD_WIDTH) {
 			return 0;
 		}
-	}
-	else
-	{
-		if (sel >= FIELD_HEIGHT)
-		{
+	} else {
+		if (sel >= FIELD_HEIGHT) {
 			return 0;
 		}
 	}
@@ -287,8 +285,8 @@ int move_overflowing(uint8_t sel, int x, int amount)
 ISR(TIMER1_COMPA_vect)
 {
 	sec++;
-	if (sec >= 60)
-	{
+
+	if (sec >= 60) {
 		sec = 0;
 		min++;
 	}
@@ -299,72 +297,44 @@ ISR(TIMER1_COMPA_vect)
  */
 ISR(PCINT2_vect)
 {
-	// Handle movement.
-	if (playing)
-	{
-		if (UP)
-		{
-			sel_y = move_overflowing(sel_y, 0, -1);
-		}
-		else if (DOWN)
-		{
-			sel_y = move_overflowing(sel_y, 0, 1);
-		}
-		else if (LEFT)
-		{
-			sel_x = move_overflowing(sel_x, 1, -1);
-		}
-		else if (RIGHT)
-		{
-			sel_x = move_overflowing(sel_x, 1, 1);
-		}
+	if (playing) {
+		handle_movement();
 	}
 
-	// Handle CHECK and FLAG.
-	if (CHECK)
-	{
-		if (!playing && !lost)
-		{
-			sec = 0;
-			min = 0;
-			playing = 1;
-		}
-		else
-		{
-			if (field_view[sel_y][sel_x] == Flag)
-				num_flags--;
-			switch (field[sel_y][sel_x])
-			{
-			case '0':
-				field_view[sel_y][sel_x] = '0';
-				break;
-			case Mine:
-				lost = 1;
-				playing = 0;
-				break;
-			}
-		}
-	}
+	handle_buttons(board[sel_y][sel_x]);
+}
 
-	if (FLAG)
-	{
-		if (playing)
-		{
-			if (field_view[sel_y][sel_x] == Blank)
-			{
-				field_view[sel_y][sel_x] = Flag;
-				num_flags += 1;
-			}
-			else if (field_view[sel_y][sel_x] == Flag)
-			{
-				field_view[sel_y][sel_x] = Blank;
-				num_flags -= 1;
-			}
-		}
-		if (lost)
-		{
-			num_flags = 0;
-			lost = 0;
-		}
-	}
+/**
+ * Sets up the timer, inputs, interruptions and custom glyphs.
+ */
+void setup()
+{
+	cli();
+
+	// Set up the timer.
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCNT1 = 0;
+	OCR1A = (TIMER_CLK / TIMER_FREQ) - 1;
+	TCCR1B |= (1 << WGM12);
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	TIMSK1 |= (1 << OCIE1A);
+
+	// Set ports as input.
+	// These will be mapped to the buttons.
+	DDRD &= ~(1 << PD1) & ~(1 << PD2) & ~(1 << PD3) & ~(1 << PD4) & ~(1 << PD6) & ~(1 << PD7);
+
+	// Toggle interruption vector for PD7, ..., PD0.
+	PCICR |= (1 << PCIE2);
+	// Toggle interruptions for every button.
+	PCMSK2 |= (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD6) | (1 << PD7);
+
+	sei();
+
+	nokia_lcd_init();
+	nokia_lcd_custom(1, CLOCK_GLYPH);
+	nokia_lcd_custom(2, SPACE_GLYPH);
+	nokia_lcd_custom(3, SELECTED_GLYPH);
+	nokia_lcd_custom(4, FLAG_GLYPH);
+	nokia_lcd_custom(5, MINE_GLYPH);
 }
