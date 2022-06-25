6f691acd8bbe695abeb7a16c13d3cb9618e3eb4e
Bernardozomer <bernardobarzottoz@gmail.com>
2022-06-25 18:06:50 -0300

    style: reorder functions

--- main.c
+++ main.c
@@ -93,117 +93,29 @@ int main()
 	}
 }
 
-/*
- * Resets the field and the view to its initial state.
- * Mines are then regenerated.
+/**
+ * Increment the timer.
  */
-void reset_board()
+ISR(TIMER1_COMPA_vect)
 {
-	for (int i = 0; i < FIELD_HEIGHT; i++) {
-		for (int j = 0; j < FIELD_WIDTH; j++) {
-			board[i][j] = (Field) {0, 0, 0};
-		}
-	}
+	sec++;
 
-	generate_mines();
-}
-
-/*
- * Fill the field with mines.
- */
-void generate_mines()
-{
-	for (int mines = 0; mines < MINE_AMOUNT;) {
-		int i = rand() % FIELD_HEIGHT;
-		int j = rand() % FIELD_WIDTH;
-
-		if (!board[i][j].mine) {
-			board[i][j].mine = 1;
-			mines++;
-		}
+	if (sec >= 60) {
+		sec = 0;
+		min++;
 	}
 }
 
 /**
- * Write the timer in MM:SS format to the screen.
+ * Handle button interruptions.
  */
-void write_timer(uint8_t x, uint8_t y)
+ISR(PCINT2_vect)
 {
-	char time_display[7];
-	sprintf(time_display, "\001%02d:%02d", min, sec);
-	nokia_lcd_set_cursor(x, y);
-	nokia_lcd_write_string(time_display, 1);
-}
-
-/**
- * Write the amount of flags used by the player to the screen.
- */
-void write_flag_count(uint8_t x, uint8_t y)
-{
-	char flags[7];
-	sprintf(flags, "%02d/%02d\004", num_flags, MINE_AMOUNT);
-	nokia_lcd_set_cursor(x, y);
-	nokia_lcd_write_string(flags, 1);
-}
-
-/**
- * Write the minefield matrix to the screen.
- */
-void write_board()
-{
-	for (int i = 0; i < FIELD_HEIGHT; i++) {
-		// Set the cursor to the start of the line.
-		nokia_lcd_set_cursor(0, i * 8);
-
-		for (int j = 0; j < FIELD_WIDTH; j++) {
-			if (i == sel_y && j == sel_x && !lost) {
-				nokia_lcd_write_string("\003", 1);
-				continue;
-			}
-
-			Field field = board[i][j];
-
-			if (!field.revealed) {
-				if (field.flagged) {
-					nokia_lcd_write_string("\004", 1);
-				} else {
-					nokia_lcd_write_string("\002", 1);
-				}
-			} else if (field.mine) {
-				nokia_lcd_write_string("\005", 1);
-			} else {
-				// TODO: write number of neighbouring bombs.
-				nokia_lcd_write_string(" ", 1);
-			}
-		}
+	if (playing) {
+		handle_movement();
 	}
-}
 
-/**
- * Write the start menu to the screen.
- */
-void write_start()
-{
-	nokia_lcd_write_string(
-		"              "
-		" AVR \005 Mines! "
-		"              "
-		" Press CHECK! "
-		"			   ",
-		1
-	);
-}
-
-/**
- * Reveal the whole board.
- */
-void reveal_board()
-{
-	for (int i = 0; i < FIELD_HEIGHT; i++) {
-		for (int j = 0; j < FIELD_WIDTH; j++) {
-			board[i][j].revealed = 1;
-		}
-	}
+	handle_buttons(&board[sel_y][sel_x]);
 }
 
 void handle_buttons(Field *sel_field)
@@ -275,29 +187,117 @@ int move_overflowing(uint8_t sel, int x, int amount)
 	return sel;
 }
 
-/**
- * Increment the timer.
+/*
+ * Resets the field and the view to its initial state.
+ * Mines are then regenerated.
  */
-ISR(TIMER1_COMPA_vect)
+void reset_board()
 {
-	sec++;
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			board[i][j] = (Field) {0, 0, 0};
+		}
+	}
 
-	if (sec >= 60) {
-		sec = 0;
-		min++;
+	generate_mines();
+}
+
+/**
+ * Reveal the whole board.
+ */
+void reveal_board()
+{
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			board[i][j].revealed = 1;
+		}
+	}
+}
+
+/*
+ * Fill the field with mines.
+ */
+void generate_mines()
+{
+	for (int mines = 0; mines < MINE_AMOUNT;) {
+		int i = rand() % FIELD_HEIGHT;
+		int j = rand() % FIELD_WIDTH;
+
+		if (!board[i][j].mine) {
+			board[i][j].mine = 1;
+			mines++;
+		}
 	}
 }
 
 /**
- * Handle button interruptions.
+ * Write the minefield matrix to the screen.
  */
-ISR(PCINT2_vect)
+void write_board()
 {
-	if (playing) {
-		handle_movement();
-	}
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
+		// Set the cursor to the start of the line.
+		nokia_lcd_set_cursor(0, i * 8);
 
-	handle_buttons(&board[sel_y][sel_x]);
+		for (int j = 0; j < FIELD_WIDTH; j++) {
+			if (i == sel_y && j == sel_x && !lost) {
+				nokia_lcd_write_string("\003", 1);
+				continue;
+			}
+
+			Field field = board[i][j];
+
+			if (!field.revealed) {
+				if (field.flagged) {
+					nokia_lcd_write_string("\004", 1);
+				} else {
+					nokia_lcd_write_string("\002", 1);
+				}
+			} else if (field.mine) {
+				nokia_lcd_write_string("\005", 1);
+			} else {
+				// TODO: write number of neighbouring bombs.
+				nokia_lcd_write_string(" ", 1);
+			}
+		}
+	}
+}
+
+/**
+ * Write the amount of flags used by the player to the screen.
+ */
+void write_flag_count(uint8_t x, uint8_t y)
+{
+	char flags[7];
+	sprintf(flags, "%02d/%02d\004", num_flags, MINE_AMOUNT);
+	nokia_lcd_set_cursor(x, y);
+	nokia_lcd_write_string(flags, 1);
+}
+
+/**
+ * Write the timer in MM:SS format to the screen.
+ */
+void write_timer(uint8_t x, uint8_t y)
+{
+	char time_display[7];
+	sprintf(time_display, "\001%02d:%02d", min, sec);
+	nokia_lcd_set_cursor(x, y);
+	nokia_lcd_write_string(time_display, 1);
+}
+
+/**
+ * Write the start menu to the screen.
+ */
+void write_start()
+{
+	nokia_lcd_write_string(
+		"              "
+		" AVR \005 Mines! "
+		"              "
+		" Press CHECK! "
+		"			   ",
+		1
+	);
 }
 
 /**
